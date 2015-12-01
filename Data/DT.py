44a0b93b9a89887f921075c1ba537e82a6686e79
Omar Farooq <omarfarooq@Omars-MacBook-Pro.local>
2015-12-01 06:00:06 -0500

    DT.py

--- Data/DT.py
+++ Data/DT.py
@@ -3,6 +3,7 @@ import sys
 import random
 import math
 from collections import defaultdict
+import itertools
 
 headers = None
 def getData(fileR,n):
@@ -41,25 +42,30 @@ def J48(arr, opp, inst, tree):
 
 def nJ48(arr, opp, inst, tree):
 	if (len(arr) != 0 and len(opp)==0):
-		tree.append(arr)
-		return arr
+		if (arr in tree) == False:
+			tree.append(arr)
+			return arr
 	elif (len(arr) == 0 and len(opp)!=0):
-		tree.append(opp)
-		return opp
+		if (opp in tree) == False:
+			tree.append(opp)
+			return opp
 
 	for item in instanceDict(arr,inst).keys():	
-		avg = avgInst(arr, inst)
-		iarr = arrOfItem(item, arr)
+		avg = avgInst(arr,inst)
+		iarr = narrOfItem(arr, avg, inst)
 		lateiarr = classDataSet(iarr,'late')
 		timeiarr = classDataSet(iarr,'time')
-		J48(lateiarr, timeiarr, inst, tree)
+		nJ48(lateiarr, timeiarr, inst, tree)
 
 
 def avgInst(narr, inst):
 	sum = 0
 	for i in instanceDict(narr,inst).keys():
-		sum+=int(i)
+		if (i !='?'):
+			sum+=int(i)
 	return sum/(len(instanceDict(narr,inst).keys()))
+
+
 
 def classInstancesCount(dataset, opt):
 	count = 0
@@ -104,12 +110,15 @@ def arrOfItem(item, arr):
 			data.append(row)
 	return data
 
-def narrOfItem(item, arr, avg):
-	data= []
-	for row in arr:
-		if item in row:
-			data.append(row)
+def arrOfItem_t(item, tree):
+	data = []
+	for arr in tree:
+		for row in arr:
+			if item in row:
+				data.append(row)
 	return data
+
+
 
 def instanceDict(arr, inst):
 	counts = defaultdict(lambda: 0)
@@ -120,6 +129,17 @@ def instanceDict(arr, inst):
 				counts[c] +=  1
 	return counts
 
+def isPure(arr):
+	ans = True
+	if (len(arr)>0):
+		x = arr[0].split(',')[-1]
+		for row in arr:
+			if (row.split(',')[-1]!=x):
+				return False
+		return True
+	else:
+		return False
+
 def findMaxPurity(arr):
 	m=0
 	for i in range(len(headers)-1):
@@ -129,7 +149,26 @@ def findMaxPurity(arr):
 	#	cols.remove(cols[m])
 	s = headers[m]
 	del headers[m]
-	return s
+	return m
+
+def narrOfItem(arr, avg, i):
+	cols = arr[0].split(',')
+	msg = []
+	for row in arr:
+		n = row.split(',')[i]
+		if (n.isdigit() and int(n) < int(avg)):
+			msg.append(row)
+	return msg
+def trainAcc(arr, pHead, tree,  k):
+	for i in pHead:
+			J48(arr,arr,i, tree)
+			n=arrOfItem_t(k.split(',')[i], tree)
+			if (isPure(n)):
+				k= k + ","+str(n[0].split(',')[-1])
+				#print k
+				return k;
+			#print k.split(',')[i], arrOfItem_t(k.split(',')[i], tree)
+
 
 def main():
 	global headers
@@ -137,9 +176,9 @@ def main():
 	#late_arr is all delayed dataset
 	#ontime_arr is all on time dataset
 	f = open(sys.argv[1],'r')
-	#t = open(sys.argv[2],'rt')
+	t = open(sys.argv[2],'rt')
 	arr=getData(f,1)
-	#art=getData(t,2)
+	art=getData(t,2)
 	headers_Copy = headers
 	late_num = classInstancesCount(arr, 'late')
 	time_num = classInstancesCount(arr, 'time')
@@ -162,40 +201,67 @@ def main():
 	#print findMaxPurity(ontime_arr)
 	#for key,value in instanceDict(ontime_arr, 1).iteritems():
 	#	print key, value
+	pHead=[]
+	for i in range(len(cols)-1):
+		pHead.append(findMaxPurity(arr))
+	print pHead
 	TP=0
 	TN=0
 	FP=0
 	FN=0
 	#print arrOfItem('AA', arr)
-	tree=[]
-	cols = arr[1].split(',')
-	for idx in range(len(headers)):
-		if (cols[idx].isdigit()!=True):
-			J48(arr,arr,idx, tree)
-		#else:
-	#print avgInst(late_arr,4)
-	#for i in tree:
-	#	print i
-	for row in arr:#change to art
-		for idx in range(len(cols)-1):
-			val=row.split(',')[idx]
-			#prob_all+=countAll[idx][val]
-			#if (idx==0):
-				#print str(val)+" "+str(countAll[idx][val])
 
-		if (2>=0):
+	#print narrOfItem(arr, avgInst(arr,2), 2)
+	tree=[]
+	k='WN,LAX,1615,2226'
+	#print k
+	for row in art:
+		k = row[:-2]
+		n=trainAcc(arr, pHead, tree, k)
+		#print str(n)[-1]
+		if (str(n).split(',')[-1]=='1'):
 			if(row.split(',')[-1]=='1'):
 				TN+=1
 			else:
 				FN+=1
-		else:
+		elif(str(n).split(',')[-1]=='0'):
 			if(row.split(',')[-1]=='0'):
 				TP+=1
 			else:
 				FP+=1
 
-	#print "----- Confusion Matrix -----\n a    b      classified as\n "+str(TP)+" "+str(FN)+""+" |    a = 0\n "+str(FP)+" "+str(TN)+""+" |    b = 1"
-	#print "Accuracy: " + str(round((float(TP+TN)/float(TP+TN+FP+FN))*100,2)) +"%"
+
+
+	#for i in pHead:
+	#	J48(arr,arr,i, tree)
+	#	n=arrOfItem_t(k.split(',')[i], tree)
+	#	if (isPure(n)):
+	#		k+=n[0].split(',')[-1]
+	#		return;
+	#	print k.split(',')[i], arrOfItem_t(k.split(',')[i], tree)
+	#J48(arr,arr,0,tree)
+	#print arrOfItem_t(k.split(',')[0], tree)	
+
+	#for t in tree:
+	#	print t
+	ntree=[]
+	cols = arr[1].split(',')
+	for idx in range(len(headers)):
+		if (cols[idx].isdigit()!=True):
+			J48(arr,arr,idx, tree)
+		else:
+			nJ48(arr,arr,idx,ntree)
+			tree.append(ntree[-1])
+	#print avgInst(late_arr,2)
+	#for row in art:#change to art
+			#prob_all+=countAll[idx][val]
+			#if (idx==0):
+				#print str(val)+" "+str(countAll[idx][val])
+
+
+
+	print "----- Confusion Matrix -----\n a    b      classified as\n "+str(TP)+" "+str(FN)+""+" |    a = 0\n "+str(FP)+" "+str(TN)+""+" |    b = 1"
+	print "Accuracy: " + str(round((float(TP+TN)/float(TP+TN+FP+FN))*100,2)) +"%"
 
 if __name__=="__main__":
 	main()
